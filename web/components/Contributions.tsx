4e15216ce84c094feaff7a783fc6c27055ea30da
tensho1026 <tensho1026@icloud.com>
2026-03-15 02:43:19 +0900

    feat:kontributions component

---
+++ web/components/Contributions.tsx
@@ -0,0 +1,10 @@
+export default function Contributions() {
+  return (
+    <div className="p-2">
+      <p className="mb-2">1,430 contributions in the last year</p>
+      <div className="border border-gray-300 h-20 w-100 rounded-lg">
+        <p>test</p>
+      </div>
+    </div>
+  );
+}
